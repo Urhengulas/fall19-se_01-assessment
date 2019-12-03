f2ee471bf6a17e8277e5e902ad09eaad2330ec9d
Urhengulas <johann.hemmann@code.berlin>
2019-12-03 14:50:22 +0100

    [ex1] Add move, done and some logging

--- ex1/main.py
+++ ex1/main.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class Board(list):
 
     win_board = [
@@ -6,8 +9,37 @@ class Board(list):
         [7, 8, 0]
     ]
 
+    move_direction = {
+        8: (-1, 0),
+        6: (0, -1),
+        2: (0, 1),
+        4: (1, 0),
+    }
+
     def __init__(self, *args, **kwargs):
+        self.zero_pos = [2, 2]
         super().__init__(*args, **kwargs)
+
+    def move(self, from_where: int) -> None:
+        zero_pos = self.zero_pos
+        direc = self.move_direction[from_where]
+
+        if from_where == 8:
+            # get position of other pirece
+            other_pos = zero_pos.copy()
+            other_pos[0] += direc[0]
+            other_pos[1] += direc[1]
+            logging.info(f"other_pos={other_pos}")
+
+            # set value at zero_pos to other_val
+            self[zero_pos[0]][zero_pos[1]] = self[other_pos[0]][other_pos[1]]
+            # set value at other_pos to 0
+            self[other_pos[0]][other_pos[1]] = 0
+
+    def done(self) -> bool:
+        if self == self.win_board:
+            return True
+        return False
 
     def __repr__(self):
         for row in self:
@@ -16,6 +48,11 @@ class Board(list):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        format="%(asctime)s.%(msecs)03d - %(module)s - %(levelname)s - %(message)s",
+        datefmt="%H:%M:%S",
+        level=logging.INFO,
+    )
 
     board = Board([
         [1, 2, 3],
@@ -27,6 +64,12 @@ if __name__ == "__main__":
         try:
             print(board)
             i = int(input("Input"))
+            board.move(i)
+            if board.done() is True:
+                print("You have won!!!")
+                break
         except KeyboardInterrupt:
             print("Reveived KeyboardInterrupt")
             break
+
+    print("Game over")
