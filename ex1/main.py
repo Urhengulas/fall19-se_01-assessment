6b086754d2cb7fedac3834b37b5f1234c01f9c58
Urhengulas <johann.hemmann@code.berlin>
2019-12-03 14:49:42 +0100

    [ex1] Add game loop, __repr__ and win_board

--- ex1/main.py
+++ ex1/main.py
@@ -1,7 +1,18 @@
 class Board(list):
 
+    win_board = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 0]
+    ]
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def __repr__(self):
+        for row in self:
+            print(row)
+        return ""
 
 
 if __name__ == "__main__":
@@ -11,4 +22,11 @@ if __name__ == "__main__":
         [4, 5, 6],
         [7, 8, 0]
     ])
-    print(board)
+
+    while True:
+        try:
+            print(board)
+            i = int(input("Input"))
+        except KeyboardInterrupt:
+            print("Reveived KeyboardInterrupt")
+            break
